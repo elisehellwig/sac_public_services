a79f8bb8c7ac54459899d2133e18e9d049e5317f
elisehellwig <echellwig@ucdavis.edu>
2024-06-11 21:02:59 -0700

    generate source points

---
+++ generate_endpoints.R
@@ -0,0 +1,31 @@
+library(osmdata)
+library(sf)
+#requires internet
+feats = available_features()
+
+highway_tags = available_tags('highway')
+#write.csv(highway_tags, 'data/highway_tags.csv', row.names = FALSE)
+walking_tags = read.csv('data/walking_tags.csv')$Value
+
+sac_merc = st_read('data/city_boundary.geojson')
+sac = st_transform(sac_merc, st_crs(4326))
+sac_bbox = st_bbox(sac)
+
+
+sac_paths = opq(sac_bbox) |>
+  add_osm_feature(key='highway', value=walking_tags) |>
+  osmdata_sf()
+
+line_vars = c('osm_id', 'name', 'highway', 'geometry')
+walking_ll = sac_paths$osm_lines[,line_vars]
+
+walking = st_transform(walking_ll, st_crs(3310))
+
+set.seed(29383)
+pts = st_line_sample(walking, density = 0.2)
+
+pts_ll = st_transform(pts, 4326)
+
+st_write(pts_ll, 'data/sac_street_sample_points.geojson')
+
+
