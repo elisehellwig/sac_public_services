d8d6f158a76e189e3930ef15e4e9db46aa563acd
elisehellwig <echellwig@ucdavis.edu>
2024-06-22 19:08:23 -0700

    code that works to query the valhalla router for a list of sets of points.

--- calculate_distance.R
+++ calculate_distance.R
@@ -3,17 +3,19 @@
 library("httr")
 library("stringr")
 library("jsonlite")
-library("pbapply")  
 library("sf")
 library("data.table")
+library('scales')
 
 source('functions.R')
 
 
-submit_req = function(request) {
+submit_save_request = function(request, fn=NA) {
   api_response = GET(request)
   
   stop_for_status(api_response)
+  
+  Sys.sleep(1)
   
   from_to_list = fromJSON(rawToChar(api_response$content))
   
@@ -21,37 +23,68 @@ submit_req = function(request) {
   
   dist_mat$departure = from_to_list$id
   
-  return(dist_mat)
+  if (is.na(fn)) {
+    return(dist_mat)
+  } else {
+    fwrite(dist_mat, fn)
+    print(fn)
+  }
   
 }
+
+create_fns = function(base_name, n, ext, fixed_width=TRUE) {
+  
+  ids = 1:n
+  
+  if (fixed_width) {
+    
+    ndig = log10(n) |> ceiling()
+    ids = formatC(ids, digits=ndig, flag='0')
+  }
+  
+  fns = paste0(base_name, '_', ids, '.', ext)
+  
+  return(fns)
+  
+}
+
+percent_done = function(df, data_path) {
+ 
+  done_fns = list.files(data_path, pattern='csv$')
+  
+  pct_num = length(done_fns)/nrow(df)
+  
+  pct_str = label_percent(accuracy=0.01)(pct_num)
+  
+  return(pct_str)
+  
+   
+}
+
 
 # if using datasci.library.ucdavis.edu as the server, make sure you are 
 # on the staff vpn (not the library vpn)
 server = "http://datasci.library.ucdavis.edu"
 port = 8002
+depart_time = '12:00'
+data_path = 'data/dist'
 
 ppr = st_read('data/Park_Restroom_Status.geojson')
 pts_list = readRDS('data/sac_street_sample_points_list.RDS')
 
+requests = sapply(pts_list, build_req_str, ppr, server, port, depart_time)
 
-request = build_req_str(test_pts, ppr, server, port, '12:00')
-system.time(mat <- submit_req(request))
+depart_hrs = gsub('[^0-9]', '', depart_time)
+base_path = file.path(data_path, paste0('distance_matrix', depart_hrs))
 
-requests = sapply(pts_list, build_req_str, ppr, server, port, '12:00')
+dist_fns = create_fns(base_path, length(pts_list), 'csv')
 
-log_fn = paste0('data/log_', Sys.time(), '.txt')
-sink()
-dist_mats = pblapply(requests, submit_req) |> rbindlist()
+request_df = data.table(fn=dist_fns, request=requests)
 
-for (time_limit in time_limits) {
-  # construct a vector of the request strings from the data
-  requests = mapply(build_req_str, address_df$Latitude, address_df$Longitude, 
-                    address_df$X, time_limit, server, port)
-  
-  # actually submit the requests to the server with valhalla
-  isochrones = pblapply(requests, submit_req)
-  isochrones = do.call(rbind, isochrones)
-  
-  # save the results
-  saveRDS(isochrones, str_glue("./data/isochrones_{time_limit}_min.rds"))
-}
+done_fns = list.files(base_path, pattern='csv$')
+
+fns_to_do = request_df[!fn %in% done_fns]
+
+msgs = mapply(submit_save_request, request_df$request, request_df$fn)
+
+percent_done(request_df, data_path)
